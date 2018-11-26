4d71d10380f2daabc86b2db8c263add66ccbe0af
Ivan <meyakovenkoj@yandex.ru>
2018-11-26 21:51:04 +0300

    Initial Commit

---
+++ DungeonGame/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  DungeonGame
+//
+//  Created by Ivan Yakovenko on 26/11/2018.
+//  Copyright © 2018 Ivan Yakovenko. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+	return 0;
+}
