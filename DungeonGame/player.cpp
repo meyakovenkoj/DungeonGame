4029f33506d6d3f5afd4a39ae0e82fdc393e6fb5
meyakovenkoj <32681800+meyakovenkoj@users.noreply.github.com>
2018-12-12 14:50:46 +0300

    fix git

--- DungeonGame/player.cpp
+++ DungeonGame/player.cpp
@@ -14,17 +14,33 @@ Player::Player(){
 	dy = 0;
 	speed = 0;
 	dir = 0;
-	//setName(_File);
 	alive = true;
 	hpMax = 100;
 	hp = hpMax;
 	exp = 0;
 	lvl = 1;
-}
-
-/*void Player::move(){
+	defence = 5;
 	
-}*/
+	sp[STRENGTH].name = "Strength";
+	sp[STRENGTH].shortname = "STR";
+	
+	sp[MANA].name = "Mana";
+	sp[MANA].shortname = "MN";
+	
+	sp[LOVKOST].name = "Lovkost'";
+	sp[LOVKOST].shortname = "LVK";
+	
+	sp[NECRO].name = "Necromant";
+	sp[NECRO].shortname = "NCR";
+	
+	sp[NONE].name = "None";
+	sp[NONE].shortname = "NN";
+	
+	for(auto & element : sp){
+		element.points = 5;
+	}
+	log << "Initialization\nDone!\n";
+}
 
 
 int Player::takeDamage(int attack) {
@@ -32,6 +48,7 @@ int Player::takeDamage(int attack) {
 	
 	if(attack > 0) {
 		hp -= attack;
+		log << "Monster \nattacked\n you with " << attack << '\n';
 		
 		if(hp <= 0) {
 			return 1;
@@ -45,37 +62,66 @@ void Player::addExperience(int _experience) {
 	exp += _experience;
 	
 	while(exp > 50) {
-		//printf("Leveled up!\n");
+		log << "Leveled up!\n";
 		exp -= 50;
 		strength += 10;
+		sp[STRENGTH].points += 10;
 		defence += 5;
 		hpMax += 10;
 		lvl++;
 	}
 }
 
-
-void Player::takeObject(){
-	
-}
-
-void Player::useObject(){
-	
-}
+//void Player::takeObject(){
+//}
+//
+//void Player::useObject(){
+//}
 
 Player::Player(float X, float Y){
 	dx = 0;
 	dy = 0;
 	speed = 0;
 	dir = 0;
-	//setName(_File);
 	alive = true;
 	hpMax = 100;
 	hp = hpMax;
 	exp = 0;
 	lvl = 1;
+	defence = 5;
 	
 	x = X;
 	y = Y;
+	
+	sp[STRENGTH].name = "Strength";
+	sp[STRENGTH].shortname = "STR";
+	
+	sp[MANA].name = "Mana";
+	sp[MANA].shortname = "MN";
+	
+	sp[LOVKOST].name = "Lovkost'";
+	sp[LOVKOST].shortname = "LVK";
+	
+	sp[NECRO].name = "Necromant";
+	sp[NECRO].shortname = "NCR";
+	
+	sp[NONE].name = "None";
+	sp[NONE].shortname = "NN";
+	
+	for(auto & element : sp){
+		element.points = 5;
+	}
+}
+
+std::string Player::setSTR(){
+	std::ostringstream EString;
+	EString << "Health: " << getHp();
+	EString << "\nExpernc: " << getExp();
+	EString << "\nLevel: " << getLvl();
+	EString << "\nDefence: " << getDef();
+	for (auto & elem : sp) {
+		EString << '\n' << elem.name << ": " << elem.points;
+	}
+	return EString.str();
 }
 
