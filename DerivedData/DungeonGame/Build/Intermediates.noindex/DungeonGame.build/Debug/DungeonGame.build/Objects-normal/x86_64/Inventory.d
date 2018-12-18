0c7b6e82795a28e45783789044510dfbc718f271
Ivan <meyakovenkoj@yandex.ru>
2018-12-18 17:52:47 +0300

    change directory

---
+++ DerivedData/DungeonGame/Build/Intermediates.noindex/DungeonGame.build/Debug/DungeonGame.build/Objects-normal/x86_64/Inventory.d
@@ -0,0 +1,2 @@
+dependencies: \
+  /Users/yakovenko/Documents/Infa/Programs_3_sem/DungeonGame/DungeonGame/Inventory.cpp
---
+++ DerivedData/DungeonGame/Build/Intermediates.noindex/DungeonGame.build/Debug/DungeonGame.build/Objects-normal/x86_64/creature.d
@@ -0,0 +1,3 @@
+dependencies: \
+  /Users/yakovenko/Documents/Infa/Programs_3_sem/DungeonGame/DungeonGame/creature.cpp \
+  /Users/yakovenko/Documents/Infa/Programs_3_sem/DungeonGame/DungeonGame/creature.hpp
